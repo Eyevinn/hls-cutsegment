4547e1442b72ae8926df4d1d2e7eb891c49daf4f
Fredrik Johansson <fredrik.johansson@eyevinn.se>
2015-11-23 11:54:46 +0100

    Moved code out into lib/clipFile.js

--- fftest.js
+++ fftest.js
@@ -6,6 +6,7 @@ var ffmpeg      = require( 'ffmpeg'      );
 var express     = require( 'express'     );
 var url         = require( 'url'         );
 var qstring     = require( 'querystring' );
+var cutter      = require( './lib/clipFile');
 
 var parser;
 var file;
@@ -40,106 +41,6 @@ function ManifestDuration(manifestfile,callback)
 		callback(total);
 	});
 
-}
-
-function ClipFile(
-	manifestfile,
-	clippoint,
-	callback
-)
-{
-	parser = m3u8.createStream();
-	file   = fs.createReadStream(manifestfile);
-
-	file.pipe(parser);
-
-	total = 0.0;
-	file_to_clip = '';
-	where_to_clip = 0.0;
-	found = false;
-
-	parser.on('item', function(item) {
-		oldtotal = total;
-		total += item.get('duration') ;
-		if( (total >= clippoint) && !found )
-		{
-			file_to_clip = item.get('uri');
-			where_to_clip = clippoint - oldtotal;
-			found = true;
-		}
-	});
-	parser.on('m3u', function(m3u) {
-		if(found)
-			callback( file_to_clip, where_to_clip );
-		else
-			callback( 'error', -1 );
-	});
-}
-
-function StitchBack(
-	old_manifest,
-	new_manifest,
-	clippoint,
-	splitfile_1,
-	splitfile_2,
-	inserted_file,
-	inserted_duration,
-	callback
-)
-{
-
-	parser_i = m3u8.createStream();
-	file_i   = fs.createReadStream(old_manifest);
-	file_i.pipe(parser_i);
-
-	parser_o = m3u8.M3U.create();
-
-	total = 0.0;
-	found = false;
-
-	parser_i.on('item', function(item) {
-		oldtotal = total;
-		dur = item.get('duration');
-		total += dur;
-		if( (total >= clippoint) && ! found )
-		{
-			found = true;
-			clpp = clippoint - oldtotal;
-			parser_o.addPlaylistItem({
-				duration : clpp,
-				uri      : splitfile_1
-			});
-			parser_o.addPlaylistItem({
-				duration : inserted_duration,
-				uri      : inserted_file
-			});
-			parser_o.addPlaylistItem({
-				duration : dur-clpp,
-				uri      : splitfile_2
-			});
-		} else {
-			fn = item.get('uri');
-			parser_o.addPlaylistItem({
-				duration : item.get('duration'),
-				uri      : fn
-			});
-		}
-
-	});
-	parser_i.on('m3u', function(m3u)
-	{
-		str = parser_o.toString();
-		fs.writeFile(new_manifest, str, function(err) {
-			if(err) {
-				console.log(err);
-				callback(false,"");
-			} else {
-				console.log("wrote new manifest ok");
-				console.log("---------------------");
-				callback(true,str);
-			}
-		});
-	} );
 }
 
 
@@ -221,7 +122,7 @@ app.get('/docut', function (req,res)
 	var ad = params['ad'];
 	var dur = params['dur'];
 
-	ClipFile( old_fn, clpp, function(ftc,wtc)
+	cutter.clipFile( old_fn, clpp, function(ftc,wtc)
 	{
 		cut_file_name(ftc, function( pth, fn, ext )
 		{
@@ -231,7 +132,7 @@ app.get('/docut', function (req,res)
 			console.log(cut1);
 			console.log(cut2);
 			console.log("---------------------");
-			StitchBack(old_fn,new_fn,clpp,cut1,cut2,ad,0,function(ok,str)
+			cutter.stitchBack(old_fn,new_fn,clpp,cut1,cut2,ad,0,function(ok,str)
 			{
 				var ff_fn = __dirname + "\\" + fn + ext;
 				console.log("---------------------");
